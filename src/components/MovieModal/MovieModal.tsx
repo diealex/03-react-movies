3fe3b86924596054ce85adff553280e728929fb6
Alex <diealex@users.noreply.github.com>
2025-05-22 13:56:10 +0200

    Modal window fixes

--- src/components/MovieModal/MovieModal.tsx
+++ src/components/MovieModal/MovieModal.tsx
@@ -1,6 +1,7 @@
 import type { Movie } from "../../types/movie";
 import css from "./MovieModal.module.css";
 import { createPortal } from "react-dom";
+import { useEffect } from "react";
 
 interface MovieModal {
   movie: Movie;
@@ -8,13 +9,39 @@ interface MovieModal {
 }
 
 export default function MovieModal({ movie, onClose }: MovieModal) {
+  const handleBackdropClick = (event: React.MouseEvent<HTMLDivElement>) => {
+    if (event.target === event.currentTarget) {
+      onClose();
+    }
+  };
+
+  useEffect(() => {
+    const handleKeyDown = (e: KeyboardEvent) => {
+      if (e.key === "Escape") {
+        onClose();
+      }
+    };
+    document.addEventListener("keydown", handleKeyDown);
+    document.body.style.overflow = "hidden";
+
+    return () => {
+      document.removeEventListener("keydown", handleKeyDown);
+      document.body.style.overflow = "";
+    };
+  }, [onClose]);
+
   return createPortal(
-    <div className={css.backdrop} role="dialog" aria-modal="true">
+    <div
+      className={css.backdrop}
+      role="dialog"
+      aria-modal="true"
+      onClick={handleBackdropClick}
+    >
       <div className={css.modal}>
         <button
           className={css.closeButton}
-          onClick={onClose}
           aria-label="Close modal"
+          onClick={onClose}
         >
           &times;
         </button>
